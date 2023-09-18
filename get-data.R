7ae7bf9237eb6840c8d8578b81ddd92ddacf38e1
CamilaAchuri <camilaachury.4@gmail.com>
2023-09-18 20:10:09 -0300

    aprehensiones sin actualizar

--- get-data.R
+++ get-data.R
@@ -4,86 +4,57 @@ library(jsonlite)
 library(tidyverse)
 library(lubridate)
 
-get_data <- function(names_data) {
-  get_token <-
-    httr::POST(
-      'https://fondocuenta.fnd.org.co/ApiOrcaDS/api/Account',
-      add_headers('content-type' = 'application/json'),
-      body = '{"Username": "OrcaDs", "Password":"ProdDS-2022Orca"}'
-    )
-  token <- content(get_token)
-  
-  end_p = Sys.Date()
-  
-  # data_save <- list(
-  #   "Inspecciones" = read_csv("data/inspecciones.csv"),
-  #   "Aprehensiones" = read_csv("data/aprehensiones.csv")
-  # )
-  # last_date <- NULL
-  # if (names_data == "Inspecciones") {
-  #   last_date <- max(data_save[[names_data]]$fecha_inspeccion, na.rm = T) 
-  # } else {
-  # last_date <- max(data_save$Aprehensiones$fecha_acta, na.rm = T)
-  # }
-  
-  url <- paste0('https://fondocuenta.fnd.org.co/ApiOrcaDS/api/', names_data, '?fechaInicial=2000-01-01&fechaFinal=', end_p)
-  #url <- paste0('https://fondocuenta.fnd.org.co/ApiOrcaDS/api/', names_data, '?fechaInicial=',last_date,'&fechaFinal=', end_p)
-  respose <-
-    httr::GET(
-      url,
-      add_headers('Authorization' = paste('Bearer', token))
-    ) 
-  
-  result <- fromJSON(content(respose, as = 'text'))
-  
-  
-  
-  
-  
-  # if (identical(result, list())) {
-  #   data <- data_save[[names_data]]
-  # } else {
-    if (names_data == "Inspecciones") {
-      result$fecha_inspeccion <- lubridate::dmy(result$fecha_inspeccion)
-      result$anio <- lubridate::year(result$fecha_inspeccion)
-      result$fecha_am <- format(result$fecha_inspeccion, "%Y-%m")
-      max(result$fecha_inspeccion)
-      result$mcipio <- trimws(result$mcipio)
-      result$depto[result$depto == "SAN ANDRES ISLAS"] <- "ARCHIPIELAGO DE SAN ANDRES, PROVIDENCIA Y SANTA CATALINA"
-      readr::write_csv(result, "data/inspecciones.csv")
-    } else {
-      result$fecha_acta <- lubridate::dmy(result$fecha_acta)
-      result$anio <- lubridate::year(result$fecha_acta)
-      result$fecha_ym <- format(result$fecha_acta, "%Y-%m")
-      max(result$fecha_acta)
-      result$mcipio <- trimws(result$mcipio)
-      clases_pr <-  c("LICOR EXTRANJERO", "LICOR NACIONAL", "CIGARRILLO EXTRANJERO", "CERVEZA EXTRANJERA", "CIGARRILLO NACIONAL", "CERVEZA NACIONAL")
-      result$cat_producto <- ifelse(result$clase_producto %in% clases_pr, "Clases principales", "Otras clases")
-      result$clase_producto[result$clase_producto %in% c("LICOR EXTRANJERO", "LICOR NACIONAL")] <- "LICORES"
-      result$clase_producto[result$clase_producto %in% c("CIGARRILLO EXTRANJERO", "CIGARRILLO NACIONAL")] <- "CIGARRILLOS"
-      result$clase_producto[result$clase_producto %in% c("CERVEZA EXTRANJERA", "CERVEZA NACIONAL")] <- "CERVEZAS"
-      result$cierre_establecimiento <- ifelse(result$cierre_establecimiento, "SÍ", "NO")
-      result$depto[result$depto == "SAN ANDRES ISLAS"] <- "ARCHIPIELAGO DE SAN ANDRES, PROVIDENCIA Y SANTA CATALINA"
-      readr::write_csv(result, "data/aprehensiones.csv")
-    # }
-    # data <- data_save[[names_data]] |> bind_rows(result)
-    
-  }
-  
-  result
-}
+options(timeout=100000)
 
-#names_data <- 
-#names_data <- "Aprehensiones"
-dir.create("data/")
-l <- lapply(c("Inspecciones", "Aprehensiones"), function(nd) {
-  df <- get_data(nd)
-  readr::write_csv(df, paste0("data/", tolower(nd), ".csv"))
-})
+get_token <-
+  httr::POST(
+    'https://fondocuenta.fnd.org.co/ApiOrcaDS/api/Account',
+    add_headers('content-type' = 'application/json'),
+    body = '{"Username": "OrcaDs", "Password":"ProdDS-2022Orca"}'
+  )
+token <- content(get_token)
 
-# readr::write_csv(result$Inspecciones, "data/inspecciones.csv")
-# read_csv("data/inspecciones.csv")
+end_p = Sys.Date()
+
+
+url <- paste0('https://fondocuenta.fnd.org.co/ApiOrcaDS/api/Inspecciones?fechaInicial=2000-01-01&fechaFinal=', end_p)
+respose <-
+  httr::GET(
+    url,
+    add_headers('Authorization' = paste('Bearer', token))
+  ) 
+
+result <- fromJSON(content(respose, as = 'text'))
+
+result$fecha_inspeccion <- lubridate::dmy(result$fecha_inspeccion)
+result$anio <- lubridate::year(result$fecha_inspeccion)
+result$fecha_am <- format(result$fecha_inspeccion, "%Y-%m")
+max(result$fecha_inspeccion)
+result$mcipio <- trimws(result$mcipio)
+result$depto[result$depto == "SAN ANDRES ISLAS"] <- "ARCHIPIELAGO DE SAN ANDRES, PROVIDENCIA Y SANTA CATALINA"
+readr::write_csv(result, "data/inspecciones.csv")
+
+
+# url_apr <- paste0('https://fondocuenta.fnd.org.co/ApiOrcaDS/api/Aprehensiones?fechaInicial=2000-01-01&fechaFinal=', end_p)
+# respose <-
+#   httr::GET(
+#     url_apr,
+#     add_headers('Authorization' = paste('Bearer', token))
+#   ) 
 # 
+# result <- fromJSON(content(respose, as = 'text'))
 # 
-# readr::write_csv(result$Aprehensiones, "data/aprehensiones.csv")
-# read_csv("data/aprehensiones.csv")
+# result$fecha_acta <- lubridate::dmy(result$fecha_acta)
+# result$anio <- lubridate::year(result$fecha_acta)
+# result$fecha_ym <- format(result$fecha_acta, "%Y-%m")
+# max(result$fecha_acta)
+# result$mcipio <- trimws(result$mcipio)
+# clases_pr <-  c("LICOR EXTRANJERO", "LICOR NACIONAL", "CIGARRILLO EXTRANJERO", "CERVEZA EXTRANJERA", "CIGARRILLO NACIONAL", "CERVEZA NACIONAL")
+# result$cat_producto <- ifelse(result$clase_producto %in% clases_pr, "Clases principales", "Otras clases")
+# result$clase_producto[result$clase_producto %in% c("LICOR EXTRANJERO", "LICOR NACIONAL")] <- "LICORES"
+# result$clase_producto[result$clase_producto %in% c("CIGARRILLO EXTRANJERO", "CIGARRILLO NACIONAL")] <- "CIGARRILLOS"
+# result$clase_producto[result$clase_producto %in% c("CERVEZA EXTRANJERA", "CERVEZA NACIONAL")] <- "CERVEZAS"
+# result$cierre_establecimiento <- ifelse(result$cierre_establecimiento, "SÍ", "NO")
+# result$depto[result$depto == "SAN ANDRES ISLAS"] <- "ARCHIPIELAGO DE SAN ANDRES, PROVIDENCIA Y SANTA CATALINA"
+# readr::write_csv(result, "data/aprehensiones.csv")
+
